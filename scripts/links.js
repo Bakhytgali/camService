aa801c241ef719b24ed4d702306961660919ba83
Bakhytgali <omgwtfwat@gmail.com>
2024-06-19 14:44:23 +0500

    Testing links

---
+++ scripts/links.js
@@ -0,0 +1,14 @@
+const link = document.querySelector("#o-nas-link");
+
+link.addEventListener("click", (event)=> {
+    event.preventDefault();
+
+    const url = link.getAttribute("data-url-change");
+    history.pushState(null, "", url);
+
+    const targetSection = document.getElementById(url);
+
+    if(targetSection) {
+        targetSection.scrollIntoView({behavior: "smooth"});
+    }
+});
